1ba5f784e39df505e527c2685bcc73d3d33d4be5
StephanBroeker <119671047+StephanBroeker@users.noreply.github.com>
2022-12-05 12:32:14 +0100

    Update __init__.py

--- abp3d/__init__.py
+++ abp3d/__init__.py
@@ -1,9 +1,18 @@
+# -*- coding: utf-8 -*- 
+
 """Module for approximating the product of the pair distribution function and
-the potential derivative for a two-dimensional suspension of active Brownian
+the potential derivative for a three-dimensional suspension of active Brownian
 particles."""
+
+__author__ = "Stephan Bröker, Raphael Wittkowski"
+__copyright__ = "Copyright (C) 2022 Stephan Bröker, Raphael Wittkowski"
+__license__ = "MIT"
+__version__ = "1.0" 
 
 from abp3d._reconstruct import *
 
 # Trick pdoc3 into documenting submodule contents as members of this module
 import abp3d._reconstruct
 __all__ = abp3d._reconstruct.__all__
+
+
