478b8263513cd6add9ed5b18f9c998cc357f0cb3
s_broe14 <s_broe14@uni-muenster.de>
2022-12-05 12:44:39 +0100

    pycodestyle for __init__.py

--- abp3d/__init__.py
+++ abp3d/__init__.py
@@ -1,18 +1,15 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 """Module for approximating the product of the pair distribution function and
 the potential derivative for a three-dimensional suspension of active Brownian
 particles."""
 
+from ._reconstruct import *
+# Trick pdoc3 into documenting submodule contents as members of this module
+from ._reconstruct import __all__ as reconstruct_all
+__all__ = reconstruct_all
+
 __author__ = "Stephan Bröker, Raphael Wittkowski"
 __copyright__ = "Copyright (C) 2022 Stephan Bröker, Raphael Wittkowski"
 __license__ = "MIT"
-__version__ = "1.0" 
-
-from abp3d._reconstruct import *
-
-# Trick pdoc3 into documenting submodule contents as members of this module
-import abp3d._reconstruct
-__all__ = abp3d._reconstruct.__all__
-
-
+__version__ = "1.0"
